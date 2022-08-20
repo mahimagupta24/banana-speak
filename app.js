5f71af0caa9d484480a022dbb58a4f651e9e7877
mahimagupta24 <mahimagupta2401@gmail.com>
2022-08-20 21:04:31 +0530

    end of it

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
 var btnTranslate=document.querySelector("#btn-Translate")
 var txtInput=document.querySelector("#txt-input")
 var outputDiv=document.querySelector("#output")
-function clickHandler(){
-outputDiv.innerText="abcsjdgail" + txtInput.value;
+//var serverURL="https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+var serverURL="https://api.funtranslations.com/translate/minion.json"
+function getTranslationURL(text){
+    return serverURL+"?"+"text=" +text
 }
+function errorHandler(error){
+    console.log("error occured",error)
+    alert("something wrong with server");
+}
+
+function clickHandler(){
+    var inputText=txtInput.value;
+    fetch(getTranslationURL(inputText))
+    .then(response=>response.json())
+    .then(json=>{
+        var translatedText=json.contents.translated;
+        outputDiv.innerText=translatedText;
+    })
+    .catch(errorHandler)
+}
+
 btnTranslate.addEventListener("click",clickHandler)
